a8f78068ec2acc0f160ca5bb2d68b1d41d27dfdc
Zhonjc2 <z1424731568@icloud.com>
2021-06-05 17:17:02 +0800

    Update
    Delete C8 on Chapter 9 branch

--- AllFonts.java
+++
@@ -1,9 +0,0 @@
-package javafxtest5;
-import javafx.scene.text.Font;
-import java.util.List;
-public class AllFonts {
-    public static void main(String[] args) {
-        List<String> l=Font.getFamilies();
-        for(String w:l)System.out.println(w);
-    }
-}
